4de91d4c3e1b0074122ee6904c0637c7f24ffa4b
Pvili2 <pvilmos2003@gmail.com>
2023-06-28 00:30:55 +0200

    Implementing UnhandleRejection event handler

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config({ path: `./config.env` });
+
+//Error handler to uncaught exceptions
+process.on('uncaughtException', err => {
+  console.log('💥 UNCAUGHT ERROR. Server shutdown...')
+  console.log(err.name, err.message);
+  process.exit(1);
+
+})
+
 const app = require('./app');
 
 const DB = process.env.DATABASE.replace(
@@ -17,6 +26,15 @@ mongoose
   .then((conn) => console.log('DB connection successfull'));
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log('Server is listening on ' + port);
 });
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message)
+  server.close(() => {
+    process.exit(1);
+  });
+})
+
+console.log(x)
