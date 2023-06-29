26a8470e7144f2e8fdac056fb129902db755e2a9
Pvili2 <pvilmos2003@gmail.com>
2023-06-29 22:48:20 +0200

    JWT (JSON web token) implementation part 1

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,20 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync')
+const jwt = require('jsonwebtoken')
 
 const signUp = catchAsync(async (req, res) => {
-    const user = await User.create(req.body);
+    const user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+    //create token
+    const token = jwt.sign({ id: user._id }, process.env.JWT_secret, { expiresIn: process.env.JWT_expires_in })
+
     res.status(200).json({
         status: 'success',
+        token,
         data: { user },
     });
 });
